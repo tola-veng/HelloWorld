d4c8ca2564bc125bf4c06beaf9dba0ef629f1864
Tola <tolaveng@hotmail.com>
2017-09-15 16:56:28 +1000

    Add project files.

---
+++ helloworld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helo World");
+            Console.ReadLine();
+            
+        }
+    }
+}
